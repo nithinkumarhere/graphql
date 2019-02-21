2cfd393db5d5a8bcaaba0254b6ce8dae97102cb7
rajpeer <rajpeer141@gmail.com>
2019-02-21 17:35:20 +0530

    no changes

--- api/resolvers/index.js
+++ api/resolvers/index.js
@@ -1,5 +1,7 @@
 import Request from '../../models/request';
 import request from 'request';
+import http from 'http';
+import { stringify } from 'querystring';
 
 export const resolvers = {
     
@@ -12,30 +14,26 @@ export const resolvers = {
         }
     },
     Mutation: {
-       async createRequest(root, { input }) {
+        async createRequest(root, { input }) {
             var kycID = input.kycId;
             console.log(kycID)
-            let status = "pending1";
             
-            await request({
-                    method: 'GET',
-                    uri: 'http://35.184.211.155:3031/api/kycAsset/'+kycID,
-                    headers: {}
-                }, function (error, response, body){
-                    if(!error && response.statusCode == 200){
-                        status = JSON.parse(body).status;
-                        console.log(status)
-                    }else{
-                        res.json("fail")
-                    }
-                })
-             
+            // request({
+            //     method: 'GET',
+            //     uri: 'http://35.184.211.155:3031/api/kycAsset/'+kycID,
+            //     headers: {}
+            // }, function (error, response, body){
+            //     if(!error && response.statusCode == 200){
+            //         status = JSON.parse(body).status;
+            //         console.log("2"+status)
+            //     }else{
+            //         res.json("fail")
+            //     }
+            // })
 
-              console.log(status)
-
+       
             var changeObject = {kycId: input.kycId, requester: input.requester, requestedOn: input.requestedOn, respondedOn: "current Time", kycStatus: status};
-            return await Request.create(changeObject);   
-           
+            return  Request.create(changeObject);
         },
         async updateRequest(root, { _id, input }) {
             return await Request.findOneAndUpdate({ _id }, input, { new: true })
