71387581be07748b6b905f980b9c2ce40eadcc51
rajpeer <rajpeer141@gmail.com>
2019-02-21 12:42:41 +0530

    somechenges

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import graphlHTTP from 'express-graphql';
 import mongoose from 'mongoose';
 import schema from './api/schema/index';
 
+
 const app = express();
 const PORT = 3000;
 
@@ -14,10 +15,13 @@ app.get('/', (req, res) => {
         msg: 'E-wallet with graphQL'
     })
 });
+
 app.use('/api', graphlHTTP({
     schema: schema,
     graphiql: true
 }));
+
+
 app.listen(PORT, () => {
     console.log(`Server is listening on PORT ${PORT}`);
 });
