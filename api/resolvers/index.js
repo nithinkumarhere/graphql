db9071095d20360c708b1d9d72ac45f54b983b27
Nithin Kumar K <nithinkumarhere@gmail.com>
2019-02-21 17:33:25 +0530

    status value appended to request

--- api/resolvers/index.js
+++ api/resolvers/index.js
@@ -1,6 +1,7 @@
 import Request from '../../models/request';
 import request from 'request';
-
+var requestify = require('requestify'); 
+var MyApp = {};
 export const resolvers = {
     
     Query: {
@@ -14,28 +15,11 @@ export const resolvers = {
     Mutation: {
        async createRequest(root, { input }) {
             var kycID = input.kycId;
-            console.log(kycID)
-            let status = "pending1";
-            
-            await request({
-                    method: 'GET',
-                    uri: 'http://35.184.211.155:3031/api/kycAsset/'+kycID,
-                    headers: {}
-                }, function (error, response, body){
-                    if(!error && response.statusCode == 200){
-                        status = JSON.parse(body).status;
-                        console.log(status)
-                    }else{
-                        res.json("fail")
-                    }
-                })
-             
-
-              console.log(status)
-
-            var changeObject = {kycId: input.kycId, requester: input.requester, requestedOn: input.requestedOn, respondedOn: "current Time", kycStatus: status};
+           var status = await requestify.get('http://35.184.211.155:3031/api/kycAsset/'+kycID).then(function (response) {
+                return response.getBody().status;
+            });
+            var changeObject = { kycId: input.kycId, requester: input.requester, requestedOn: input.requestedOn, respondedOn: "current Time", kycStatus: status};
             return await Request.create(changeObject);   
-           
         },
         async updateRequest(root, { _id, input }) {
             return await Request.findOneAndUpdate({ _id }, input, { new: true })
